24b83acdf01e88e7d4a6d6676bf7681089ce1e3a
Alex Delooze <alex.s.delooze@gmail.com>
2021-01-28 17:08:44 +0000

    trying to fix my mistake with breaking the deploys

--- docs/wlcm_to_cdv.md
+++
@@ -1,6 +0,0 @@
----
-id: testAlex
-title: Welcome to CADvanced
----
-
-# CADvanced
--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,6 @@
 module.exports = {
   someSidebar: {
     Docusaurus: ['doc1', 'doc2', 'doc3'],
-    Features: ['mdx', 'wlcm_to_cdv'],
+    Features: ['mdx'],
   },
 };
