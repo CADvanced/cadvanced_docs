51d645f6156040cc6a00f6c4c39b679d0b490088
Alex Delooze <alex.s.delooze@gmail.com>
2021-01-30 17:37:39 +0000

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -1,8 +1,5 @@
 module.exports = {
   someSidebar: {
-    Introduction: ['intro'],
-    Docusaurus: ['doc1', 'doc2', 'doc3'],
-    Features: ['mdx'],
-    AlexTest: ['alexiscool']
+    Introduction: ['intro']
   },
 };
